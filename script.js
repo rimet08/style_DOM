f988b3e3e13ae78e8071deb2015cc0c28f15b0ef
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-11-03 12:06:48 +0100

    Create

---
+++ script.js
@@ -0,0 +1,9 @@
+// získání referencí na HTML prvky
+const cpxBarvaTextu = document.getElementById('cpxBarvaTextu');
+const cpxBarvaPozadi = document.getElementById('cpxBarvaPozadi');
+const rngVelikostTextu = document.getElementById('rngVelikostTextu');
+const spnVelikostTextu = document.getElementById('spnVelikostTextu');
+const selFontFamily = document.getElementById('selFontFamily');
+const divPreviewBox = document.getElementById('divPreviewBox');
+const chkShowPreview = document.getElementById('chkShowPreview');
+
